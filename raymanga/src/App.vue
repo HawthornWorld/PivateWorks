27ab41e9e58a145c2bcddcdebf2374a6bf2794f7
crystal <ymaa@ctrip.com>
2019-03-21 00:42:52 +0800

    create app

---
+++ raymanga/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="app">
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld,
+  },
+  mounted() {
+    this.setRemRoot()
+  },
+  methods: {
+    setRemRoot() {
+      var fnSet = function() {
+        var availWidth =
+          window.innderWidth ||
+          document.documentElement.clientWidth ||
+          document.body.clientWidth;
+        var rootFontSize =
+          (availWidth =
+            750 < (availWidth = availWidth < 320 ? 320 : availWidth)
+              ? 750
+              : availWidth) /
+          540 *
+          20;
+        document.getElementsByTagName('html')[0].style.fontSize =
+          rootFontSize + 'px';
+      };
+      fnSet();
+      window.onresize = fnSet;
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ raymanga/vue.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    css: {
+      extract: false
+    },
+    productionSourceMap: false,
+    // chainWebpack: config => {
+    //   config.plugin("html").tap(args => {
+    //     args[0].minify = false;
+    //     return args;
+    //   });
+    // },
+    devServer: {
+      disableHostCheck: true,
+      host: 'localhost',
+      port: 8080,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'PUT, GET, POST, DELETE, OPTIONS',
+        'Access-Control-Allow-Headers': "X-Requested-With",
+        'Access-Control-Allow-Headers': 'Content-Type',
+      }
+    }
+  }
