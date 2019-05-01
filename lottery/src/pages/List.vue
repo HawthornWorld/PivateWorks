385eb13b45ee1433df3930ac58133c8341b9fc11
liuruochen <liurchen@hotmail.com>
2019-05-01 18:56:44 +0800

    save

--- lottery/src/pages/List.vue
+++ lottery/src/pages/List.vue
@@ -1,9 +1,12 @@
 <template>
 	<div class="list-wrapper">
-    </div>
+		<nav-bar></nav-bar>
+	</div>
 </template>
 
 <script>
+import NavBar from "../components/NavBar.vue";
+
 export default {
 	name: "list",
 	data() {
@@ -11,17 +14,19 @@ export default {
 	},
 	computed: {},
 	methods: {},
-	components: {}
+	components: {
+		NavBar
+	}
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
 .list-wrapper {
-    width: 100%;
-    height: 100%;
-    padding: 0;
-    margin: 0;
-    background: #6829c8;
+	width: 100%;
+	height: 100%;
+	padding: 0;
+	margin: 0;
+	background: #6829c8;
 }
 </style>
