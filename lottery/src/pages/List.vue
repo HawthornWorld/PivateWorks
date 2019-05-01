0710bccb414b622b0dd31b9c35b33c5c41ea78e5
liuruochen <liurchen@hotmail.com>
2019-05-01 16:42:18 +0800

    pu

--- lottery/src/pages/List.vue
+++ lottery/src/pages/List.vue
@@ -1,6 +1,5 @@
 <template>
 	<div class="list-wrapper">
-        <h2>hello list</h2>
     </div>
 </template>
 
@@ -18,5 +17,11 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
-
+.list-wrapper {
+    width: 100%;
+    height: 100%;
+    padding: 0;
+    margin: 0;
+    background: #6829c8;
+}
 </style>
